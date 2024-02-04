689b67a443fcfb4dd7919453454bac1cdf1683cd
Acutapugione <acuta.pugione@gmail.com>
2024-02-04 17:45:43 +0200

    . .

--- app/forms/message.py
+++ app/forms/message.py
@@ -1,11 +1,27 @@
-from wtforms import Form, BooleanField, StringField, PasswordField, validators, SubmitField, SelectField, TextAreaField
+from wtforms import (Form, BooleanField, StringField, PasswordField,
+                     validators, SubmitField, SelectField, TextAreaField)
 from db import Session, Department, Executor, Order
 from sqlalchemy import select
 
 with Session.begin() as session:
     class MessageForm(Form):
-        phone = StringField('Номер телефона', [validators.Length(min=13, max=13)], default="+380")
-        target = SelectField('Місце проведення', choices=[str(x) for x in session.scalars(select(Department)).all()])
-        executor = SelectField('Виконавець', choices=[str(x) for x in session.scalars(select(Executor)).all()])
-        submit = SubmitField('Оформити заявку', [validators.DataRequired()])
-        description = TextAreaField('Опис заявки', [validators.Length(min=10, max=255)])
+        phone = StringField(
+            'Phone', [
+                validators.Length(min=13, max=13)], default="+380"
+        )
+        target = SelectField(
+            'Місце проведення',
+            choices=[str(x) for x in session.scalars(select(Department)).all()]
+        )
+        executor = SelectField(
+            'Виконавець',
+            choices=[str(x) for x in session.scalars(select(Executor)).all()]
+        )
+        submit = SubmitField(
+            'Оформити заявку',
+            [validators.DataRequired()]
+        )
+        description = TextAreaField(
+            'Опис заявки',
+            [validators.Length(min=10, max=255)]
+        )
